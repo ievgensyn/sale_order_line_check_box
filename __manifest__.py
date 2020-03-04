a3e79a62d74e07fe303dd7919a8cf918acf306a9
Ievgen Synchyshyn <yevgens@web.de>
2020-03-04 22:01:25 +0200

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Sale order: shipping from SOL ',
+
+    'summary': 'Sale Order Shipping From SO line',
+
+    'author': 'Ievgen Synchyshyn',
+    'website': 'https://example.com/',
+
+    'category': 'Sales',
+    'license': 'Other proprietary',
+    'version': '13.0.1.0.1',
+
+    'depends': ['base', 'sale', ],
+    'data': [
+        'views/sale_order_view.xml',
+    ],
+
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
